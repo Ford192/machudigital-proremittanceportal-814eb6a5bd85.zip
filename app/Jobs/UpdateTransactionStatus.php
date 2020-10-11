23236e8f463bd70a52270f7d404b8c9b7af2821f
zeepay-tech <technical@myzeepay.com>
2020-10-11 22:56:55 +0000

    add request payload to cash pick up status updates

--- app/Jobs/UpdateTransactionStatus.php
+++ app/Jobs/UpdateTransactionStatus.php
@@ -40,6 +40,13 @@ class UpdateTransactionStatus implements ShouldQueue
         Log::info("[UpdateTransactionStatus][".$this->transaction->id."]\t Called...");
         try{
 
+            $user = \App\User::find($this->transaction->bank_officer);
+
+            if (empty($user)){
+                Log::info("[UpdateTransactionStatus][".$this->transaction->id."]\t User is empty exiting...");
+                return;
+            }
+
             $ptoken = Ptoken::where('status',1)->orderBy('created_at','desc')->first();
 
             Log::info("[UpdateTransactionStatus][".$this->transaction->id."]\t Token Object...",(!empty($ptoken) ? $ptoken->toArray() : []));
@@ -57,7 +64,22 @@ class UpdateTransactionStatus implements ShouldQueue
 
             Log::info("[UpdateTransactionStatus][".$this->transaction->id."]\t final URL...".$url);
 
-            $httpResponse = $httpClient->post($url,['headers' => ["Authorization" => $ptk]]);
+            $httpResponse = $httpClient->post($url,[
+                'headers' => ["Authorization" => $ptk],
+                'json' => [
+                    "remarks" => "funds paid out successfully",
+                    "identification" => [
+                        "type" => strtoupper($this->transaction->rec_id_type),
+                        "number" => $this->transaction->rec_id_number,
+                    ],
+                    "branch" => $user->bank_branch,
+                    "payer_code" => null,
+                    "completed_on" => \Carbon\Carbon::now()->format("Y-m-d\TH:i:s\Z"),
+                    "action" => "complete",
+                    "employee" => $user->name,
+                    "hostname" => "remit-portal",
+                ]
+            ]);
 //            $requests = $httpClient->request('POST', "https://shop.digitaltermination.$httpResponse/api/transactions/cash-pick-ups/call-back/".$this->transaction->transaction_id);
             Log::info("[UpdateTransactionStatus]\t HTTP Response Status Code: ".$httpResponse->getStatusCode());
             Log::info("[UpdateTransactionStatus]\t HTTP Response Body: ".$httpResponse->getBody());
